0881abf6d6121f06daf9ac951e3dcf183f99f150
ARSAN <chris@arsancg.com>
2023-09-07 16:47:41 -0600

    init spawn manager

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private float speed = 30.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
