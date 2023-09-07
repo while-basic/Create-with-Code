0f24e6b7bb7e311f4b8c9fa3fe4158e4f574af51
ARSAN <chris@arsancg.com>
2023-09-07 17:11:06 -0600

    minor

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30.0f;
+    private float speed = 30;
 
     // Start is called before the first frame update
     void Start()
