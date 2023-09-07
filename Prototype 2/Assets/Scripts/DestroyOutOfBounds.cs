fc04d83f9fd5f13c28f7cf1dd8ba81bcedac0c5f
ARSAN <chris@arsancg.com>
2023-09-07 03:00:41 -0600

    minor

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 40;
+    private float topBound = 10;
     private float lowerBound = -10;
 
     // Start is called before the first frame update
