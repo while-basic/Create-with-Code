1d2b21ef2a339fa25ac0b5dc7f9edfcb5a4f5c66
ARSAN <chris@arsancg.com>
2023-09-08 14:40:03 -0600

    tweaked gravity and jump force

--- Jump Force/Assets/Scripts/PlayerController.cs
+++ Jump Force/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb; // player's rigid body, not visibile to outside methods
+    public float jumpForce; // players jumping force
+    public float gravityModifier; // controlls gravity
+
 
     // Start is called before the first frame update
     void Start()
@@ -12,6 +15,7 @@ public class PlayerController : MonoBehaviour
         // Make player jump at start
         playerRb = GetComponent<Rigidbody>(); // gets player's rigid body component and takes no parameters
         // playerRb.AddForce(Vector3.up * 1000); // applies force to player's position on z-axis and multiplies by 1,000 when start is pressed
+        Physics.gravity *= gravityModifier; // pulls gravity from Unity, multiply equals the gravity modifier
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class PlayerController : MonoBehaviour
         // make player jump only when spacebar is pressed
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse); // applies different types of forces
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // applies different types of forces
         }
     }
 }
