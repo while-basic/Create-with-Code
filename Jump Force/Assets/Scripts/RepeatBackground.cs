486f1a116553cbae895888fc6d2ea0475c26f034
ARSAN <chris@arsancg.com>
2023-09-08 15:47:57 -0600

    reset position of background

--- Jump Force/Assets/Scripts/RepeatBackground.cs
+++ Jump Force/Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos; //position object starts at
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;   
     }
 
     // Update is called once per frame
     void Update()
     {
+        // reset to make background go back to start position
+        if (transform.position.x < startPos.x - 50) // offsetfor background repeat
+        {
+            transform.position = startPos;// reset back to position of variable
+        }
         
     }
 }
