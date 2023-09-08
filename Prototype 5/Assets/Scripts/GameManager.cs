a1f21d31b2e75b43579727459ff72b317aad9924
ARSAN <chris@arsancg.com>
2023-09-08 08:54:03 -0600

    update score when spawning

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -31,6 +31,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
+            UpdateScore(5);
         }
+    }
+
+    void UpdateScore(int scoreToAdd) 
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
