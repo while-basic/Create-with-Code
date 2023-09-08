d1238d95ebc423148d5f9fd3ca70e67c8e1f710a
ARSAN <chris@arsancg.com>
2023-09-08 14:33:19 -0600

    added spacebar for jumping

--- Jump Force/Assets/Scripts/PlayerController.cs
+++ Jump Force/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb; // player's rigid body, not visibile to outside methods
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Make player jump at start
+        playerRb = GetComponent<Rigidbody>(); // gets player's rigid body component and takes no parameters
+        // playerRb.AddForce(Vector3.up * 1000); // applies force to player's position on z-axis and multiplies by 1,000 when start is pressed
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // make player jump only when spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse); // applies different types of forces
+        }
     }
 }
