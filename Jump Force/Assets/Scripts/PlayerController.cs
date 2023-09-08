7f9d4670856752a81b859c1b435786e4226e7be0
ARSAN <chris@arsancg.com>
2023-09-08 15:14:48 -0600

    added reference to obstacle for spawning

--- Jump Force/Assets/Scripts/PlayerController.cs
+++ Jump Force/Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         // Make player jump at start
         playerRb = GetComponent<Rigidbody>(); // gets player's rigid body component and takes no parameters
-        // playerRb.AddForce(Vector3.up * 1000); // applies force to player's position on z-axis and multiplies by 1,000 when start is pressed
+        // playerRb.AddForce(Vector3.up * 1000); // applies force to player's position on (?)-axis and multiplies by 1,000 when start is pressed
         Physics.gravity *= gravityModifier; // pulls gravity from Unity, multiply equals the gravity modifier
     }
 
--- Jump Force/Assets/Scripts/SpawnManager.cs
+++ Jump Force/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    
+    public GameObject obstactlePrefab; // reference to obstacle for spawning prefab game objects
+    private Vector3 spawnPos = new Vector3(25, 0, 0); // spawn position for obstacles
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // creates prefab using instantiate methods (creates copies)
+        Instantiate(obstactlePrefab, spawnPos, obstactlePrefab.transform.rotation);
     }
 
     // Update is called once per frame
