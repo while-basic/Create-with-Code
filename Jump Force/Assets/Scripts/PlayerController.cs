66e8adfbf2db111d87aa065c279664328303416e
ARSAN <chris@arsancg.com>
2023-09-08 18:35:34 -0600

    added animation with corrected jump movements

--- Jump Force/Assets/Scripts/PlayerController.cs
+++ Jump Force/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb; // player's rigid body, not visibile to outside methods
+    private Rigidbody playerRb;
+    private Animator playerAnim; // player's rigid body, not visibile to outside methods
     public float jumpForce; // players jumping force
     public float gravityModifier; // controlls gravity
     public bool isOnGround = true; // checks player is on ground
@@ -16,6 +17,8 @@ public class PlayerController : MonoBehaviour
         // Make player jump at start
         playerRb = GetComponent<Rigidbody>(); // gets player's rigid body component and takes no parameters
         // playerRb.AddForce(Vector3.up * 1000); // applies force to player's position on (?)-axis and multiplies by 1,000 when start is pressed
+
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier; // pulls gravity from Unity, multiply equals the gravity modifier
     }
 
@@ -26,7 +29,8 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // applies different types of forces
-            isOnGround = false; 
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); // activates jump animation trigger event
         }
     }
 
